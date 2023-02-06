f2bdd699bc3ac9e399c6556dd9db9d1c8bd55b25
sheelkumar558 <sheelkumar558@gmail.com>
2023-02-06 23:41:11 +0530

    add mern server

--- mernTodo/server/index.js
+++ mernTodo/server/index.js
@@ -5,12 +5,13 @@ const cors = require("cors");
 const TodoRoutes = require("./routes/todoRoutes");
 const app = express();
 //use express.json() to get data into json formate
+
+const PORT = process.env.PORT || 5500;
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello");
 });
-const PORT = process.env.PORT || 5500;
-app.use(cors());
 mongoose
   .connect(process.env.DB_CONNECT)
   .then(() => console.log("database connected"))
